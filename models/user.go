1f27a0aeea225db9bde4a0ab86fa10a764991f52
luenci <lucarful@gmail.com>
2022-03-10 18:03:03 +0800

    feat: 新增user字段和测试文件

--- models/user.go
+++ models/user.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"github.com/google/uuid"
 	ms "github.com/luenci/oauth2/store/mysql"
 	"gorm.io/gorm"
 )
@@ -8,8 +9,14 @@ import (
 // User struct 用户信息表
 type User struct {
 	gorm.Model
+	UserId   string `gorm:"type:varchar(36);unique_index;not null"`
 	Name     string `gorm:"type:varchar(100);not null"`
 	Password string `gorm:"type:varchar(100);not null"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	tx.Statement.SetColumn("user_id", uuid.New().String())
+	return
 }
 
 func (u *User) TableName() string {
@@ -26,8 +33,12 @@ func (u *User) GetAllUsers() ([]User, error) {
 	return users, err
 }
 
+func (u *User) GetUserID(name, password string) error {
+	return ms.MysqlDB.Select("user_id").Where("name = ? and password = ?", name, password).Find(&u).Error
+}
+
 func (u *User) Create() error {
-	return ms.MysqlDB.Create(u).Error
+	return ms.MysqlDB.Create(&u).Error
 }
 
 func (u *User) Update() error {
---
+++ models/user_test.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/luenci/oauth2/config"
+
+	"github.com/luenci/oauth2/store/mysql"
+)
+
+func TestUser_Create(t *testing.T) {
+	type fields struct {
+		Name     string
+		Password string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+	}{
+		{name: "create_user", fields: fields{Name: "test", Password: "test"}},
+	}
+	config.InitConf()
+	conf := config.GetConf()
+
+	mysql.InitMysqlClient(conf.Mysql.DSN, mysql.WithMaxIdleConnections(conf.Mysql.MaxIdleConnections),
+		mysql.WithMaxOpenConnections(conf.Mysql.MaxOpenConnections),
+		mysql.WithMaxConnectionLifeTime(conf.Mysql.MaxConnectionLifeTime))
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			usr := NewUser()
+			usr.Name = tt.fields.Name
+			usr.Password = tt.fields.Password
+			if err := usr.Create(); err != nil {
+				t.Errorf("Create() error = %v", err)
+			}
+		})
+	}
+}
